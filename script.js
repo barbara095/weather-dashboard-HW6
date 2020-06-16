e91a8942ddefcb37bbb03daebbb027a94902af5a
Barbs <barbarap095@gmail.com>
2020-06-16 17:06:35 +1000

    Commit changes

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-var APIKey = "3faa5d1f7e1b0157f518296126002213";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Melbourne,Victoria,Australia&appid=" + APIKey;
+$("#city-input").on("click", function (event) {
+    event.preventDefault();
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-  })
+    var APIKey = "3faa5d1f7e1b0157f518296126002213";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Melbourne,Victoria,Australia&appid=" + APIKey;
+
+    var cityInput = $("#city-input").val().trim();
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        $("city-view").text(JSON.stringify(response));
+
+        var cityDiv = $("<div class='city'>");
+        cityDiv = cityInput;
+    })
+
+})
