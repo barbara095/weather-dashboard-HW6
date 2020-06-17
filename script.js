a0436eab8c8204bbbdabc733a9f68935c3723d56
Barbs <barbarap095@gmail.com>
2020-06-17 15:32:39 +1000

    Add variables to js

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-$("#city-input").on("click", function (event) {
-    event.preventDefault();
+$(document).ready(function () {
+    // HTML variables
 
-    var APIKey = "3faa5d1f7e1b0157f518296126002213";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Melbourne,Victoria,Australia&appid=" + APIKey;
+    var currentDate = moment().format("dddd, MMMM Do");
+    var listCities = $(".list-cities");
+    var citySearch = $("#city-input");
+    var clearCities = $("#clear-cities");
 
-    var cityInput = $("#city-input").val().trim();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
 
-        $("city-view").text(JSON.stringify(response));
+    // Array Variables
 
-        var cityDiv = $("<div class='city'>");
-        cityDiv = cityInput;
+    var searchedCities = [];
+    var citiesArray = [];
+
+
+    $("#submit").on("click", function () {
+
+        renderForecast();
     })
 
+    function renderForecast (){
+        event.preventDefault();
+        
+        $("#show-cities").empty();
+
+        var APIKey = "3faa5d1f7e1b0157f518296126002213";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            $("city-view").text(JSON.stringify(response));
+
+            var cityDiv = $("<div class='city'>");
+            cityDiv.push(city);
+        })
+
+}
+
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    city.push(listCities);
+
+
+})
 })
