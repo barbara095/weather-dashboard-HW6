50305da4c50e0c2532a1274447067a92a8cc5969
Barbs <barbarap095@gmail.com>
2020-06-21 21:50:37 +1000

    Commit changes

--- script.js
+++ script.js
@@ -2,50 +2,103 @@ $(document).ready(function () {
     // HTML variables
 
     var currentDate = moment().format("dddd, MMMM Do");
-    var listCities = $(".list-cities");
-    var citySearch = $("#city-input");
+    var listCities = $("#show-cities");
+
+    var currentWeather = $(".current-forecast");
+    var futureWeather = $(".future-forecast");
+    var dateDiv = $("<div>");
+    var weatherHeading = $("<h1>");
+    var tempDiv = $("<div>");
+    var humidityDiv = $("<div>");
+    var windSpeedDiv = $("<div>");
+    var uvIndexDiv = $("<div>");
     var clearCities = $("#clear-cities");
-
-
 
     // Array Variables
 
-    var searchedCities = [];
-    var citiesArray = [];
+    var searchedCitiesArray = [];
+    var citySearch = [];
+
+    var APIKey = "3faa5d1f7e1b0157f518296126002213";
 
 
-    $("#submit").on("click", function () {
-
-        renderForecast();
-    })
-
-    function renderForecast (){
+    $("#submit").on("click", function (event) {
         event.preventDefault();
         
-        $("#show-cities").empty();
+        var city = $("#city-input").val();
 
-        var APIKey = "3faa5d1f7e1b0157f518296126002213";
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+        if (city !== "") {
+            $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + APIKey,
+                method: "GET"
+            }).then(function (response) {
+                // Pull through weather attributes from API
+                console.log(response);
+                currentTemp = Math.floor(response.main.temp);
+                weatherIcon = response.weather.icon;
+                currentHumidity = response.main.humidity;
+                currentWindSpeed = response.wind.speed;
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
+                currentWeather.attr('class', 'display-current');
+                dateDiv.attr('class', 'display date');
+                weatherHeading.attr("Forecast for: " + citySearch + "" + currentDate);
+                tempDiv.attr('class', 'display current temp').text("Current temperature: " + currentTemp + "&#8451");
+                humidityDiv.attr('class', 'display current humidity').text("Humidity: " + currentHumidity + "%");
+                windSpeedDiv.attr('class', 'display current wind speed').text("Wind Speed: " + currentWindSpeed + "KMPH");
+                
+                // Append weather data to respective div tags
+                
 
-            $("city-view").text(JSON.stringify(response));
+            })
+        }
+    init();
 
-            var cityDiv = $("<div class='city'>");
-            cityDiv.push(city);
-        })
+    function searchedCities() {
+        if (searchedCitiesArray.length !== 0) {
+            listCities.html("");
+        }
+        for (var i = 0; i < searchedCitiesArray.length; i++) {
+            citySearch = searchedCitiesArray[i];
 
-}
+            var searchList = $("<li>").attr("class", "list-cities").data('index', i);
+            listCities.append(searchList);
+        };
 
-$("#submit").on("click", function(event) {
-    event.preventDefault();
+    };
 
-    city.push(listCities);
+    function clearSearchHistory() {
+
+        searchedCitiesArray = [];
+        listCities.text("");
+        localStorage.clear();
+
+    };
+
+    function init() {
+
+        var storedCities = JSON.parse(localStorage.getItem('cities'));
+        if (storedCities !== null) {
+            searchedCitiesArray = storedCities;
+        }
+
+        searchedCities(); 
+    };
+
+    function locallyStoredCities() {
+        // Stringify and set "cities" key in localStorage to recentlySearchedCitiesArray
+        localStorage.setItem("cities", JSON.stringify(searchedCitiesArray));
+    };
+
+        locallyStoredCities();
+        searchedCities();
+
+    });
 
 
-})
-})
+    clearCities.on("click", function (event) {
+        event.preventDefault();
+        clearSearchHistory();
+        citySearch = ("");
+
+    });
+});
